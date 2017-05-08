5e7c13f9668cc1d72f0d4846e16f1c36430b25e3
Livilian <olivia.sart@hotmail.com>
2017-05-08 14:48:02 +0200

    Project created

---
+++ routes/auth-routes.js
@@ -0,0 +1,63 @@
+/*jshint esversion:6*/
+const express = require("express");
+const authRoutes = express.Router();
+
+//User model
+const User = require("../models/user");
+
+// Bcrypt to encrypt passwords
+const bcrypt     = require("bcrypt");
+const bcryptSalt = 10;
+
+//we require passport to use it in our routes
+const passport      = require("passport");
+
+authRoutes.get("/signup", (req, res, next) => {
+  res.render("auth/signup.ejs");
+});
+
+authRoutes.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt     = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = User({
+      username: username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+});
+
+authRoutes.get("/login", (req, res, next) => {
+  res.render("auth/login.ejs");
+});
+
+authRoutes.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+module.exports = authRoutes;
